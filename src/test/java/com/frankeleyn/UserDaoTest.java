7aec78e3caae8cf4c8fadcfc265236894fa9cc9c
frankeleyn <2582726641@qq.com>
2022-01-15 10:44:36 +0800

    初始化 Spring Boot

---
+++ src/test/java/com/frankeleyn/UserDaoTest.java
@@ -0,0 +1,17 @@
+package com.frankeleyn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Frankeleyn
+ * @date 2022/1/15 10:42
+ */
+@SpringBootTest
+public class UserDaoTest {
+
+    @Test
+    public void test1() {
+        System.out.println("Hello");
+    }
+}
